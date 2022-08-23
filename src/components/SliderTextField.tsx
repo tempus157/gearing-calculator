806be2071a345b8831fc65f13e547ea06585ff05
tempus157 <tempus157@gmail.com>
2022-08-24 00:08:41 +0900

    Extract SliderTextField component

---
+++ src/components/SliderTextField.tsx
@@ -0,0 +1,72 @@
+import { Grid, Input, Slider, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+interface SliderInputProps {
+  label: string;
+  initialValue: number;
+  min: number;
+  max: number;
+  step: number;
+  marks?: boolean;
+}
+
+function clamp(value: number, min: number, max: number) {
+  return Math.min(Math.max(value, min), max);
+}
+
+function round(value: number) {
+  return Math.round(value * 100) / 100;
+}
+
+function SliderTextField({
+  label,
+  initialValue,
+  min,
+  max,
+  step,
+  marks,
+}: SliderInputProps) {
+  const [value, setValue] = useState<number | null>(initialValue);
+
+  function onSliderChange(event: Event, newValue: number | number[]) {
+    setValue(newValue as number);
+  }
+
+  function onInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    if (event.target.value === "") {
+      setValue(null);
+      return;
+    }
+    const newValue = Number(event.target.value);
+    setValue(clamp(round(newValue), min, max));
+  }
+
+  return (
+    <>
+      <Typography>{label}</Typography>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs>
+          <Slider
+            value={value ?? min}
+            onChange={onSliderChange}
+            min={min}
+            max={max}
+            step={step}
+            marks={marks}
+          />
+        </Grid>
+        <Grid item width={100}>
+          <TextField
+            value={value}
+            onChange={onInputChange}
+            type="number"
+            size="small"
+            error={value === null}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default SliderTextField;
