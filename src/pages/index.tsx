4a3fc41211c73344b42a07e31b7021746898ddcf
tempus157 <tempus157@gmail.com>
2022-08-23 22:10:20 +0900

    Make up SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import TitleBar from "@/components/TitleBar";
 import { Button } from "@mui/material";
 import type { NextPage } from "next";
+import { NextSeo } from "next-seo";
 
 const Home: NextPage = () => {
   return (
     <>
+      <NextSeo
+        title="Gearing Calculator"
+        description="A simple gearing calculator for Forza Horizon"
+      />
       <TitleBar text="Gearing Calculator" />
       <h1>Lorem Ipsum</h1>
       <Button variant="contained">OK</Button>
